8be81e510b21d3fd2b1c8405f6f5a41e8f1dab89
Adam Lipshay <adam@Adams-MacBook-Air.local>
2024-09-04 11:33:11 -0400

    Adding neoconf

--- init.lua
+++ init.lua
@@ -317,6 +317,7 @@ require('lazy').setup({
   },
 
   -- LSP Plugins
+  { 'folke/neoconf.nvim' },
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
@@ -487,6 +488,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -511,6 +513,8 @@ require('lazy').setup({
           },
         },
       }
+
+      require('neoconf').setup()
 
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
